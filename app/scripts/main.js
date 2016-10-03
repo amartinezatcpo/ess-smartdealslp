17c87f6508c3a3d6b12a402b9f07e8c7ca0ccb68
Tyler Reitz <tdreitz@gmail.com>
2016-10-03 09:14:01 -0700

    adds select box & styles and blue text clr

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -177,4 +177,23 @@ $(document).ready(function () {
     init(data);
     return mySwiper = new Swiper('.swiper-container', cfg.swiper);
   }, 'json')
+
+  // Default dropdown action to show/hide dropdown content
+  $('.js-dropp-action').click(function(e) {
+    e.preventDefault();
+    $(this).toggleClass('js-open');
+    $(this).parent().next('.dropp-body').toggleClass('js-open');
+  });
+
+  // Using as fake input select dropdown
+  $('label').click(function() {
+    $(this).addClass('js-open').siblings().removeClass('js-open');
+    $('.dropp-body,.js-dropp-action').removeClass('js-open');
+  });
+
+  // get the value of checked input radio and display as dropp title
+  $('input[name="dropp"]').change(function() {
+    var value = $('input[name=\'dropp\']:checked').val();
+    $('.js-value').text(value);
+  });
 })
