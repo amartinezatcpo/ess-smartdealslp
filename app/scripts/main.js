ecdfaf9f8cdc7cf08ec0d00c022221a388a33dec
Tyler Reitz <tdreitz@gmail.com>
2016-10-03 08:08:54 -0700

    updates to main.js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,8 +1,8 @@
 /* eslint multiline-ternary: ["warn", "always"], no-multiple-empty-lines: "off", no-implicit-globals: 0, no-tabs: "warn"*/
 
-/**
+/** **********************************************************
  * Configs
- */
+ ***********************************************************/
 
  var swiperOpts = {
    prevButton: '.swiper-button-prev',
@@ -22,6 +22,9 @@
    }
  }
 
+ var templateSwpr = '<li class="swiper-slide"><div class="card"><a class="card-link" href="#"><div class="card-media"></div><div class="item-info"><div class="item-name"><span class="bold">{{description}}</span></div><div class="item-sku"><span class="font_small uppercase">{{skuid}}</span></div><div class="item-price"><span class="bold">${{price}} /EA</span></div></div></a></div></li>';
+ var templateGrid = '<li class="prod-grid__item"><div class="card"><a class="card-link" href="#"><div class="card-media"></div><div class="item-info"><div class="item-name"><span class="bold">{{description}}</span></div><div class="item-sku"><span class="font_small uppercase">{{skuid}}</span></div><div class="item-price"><span class="bold">${{price}} /EA</span></div></div></a></div></li>';
+
 var cfg = {
   swiper: swiperOpts,
   ajx: {
@@ -30,49 +33,56 @@ var cfg = {
       'Save on Craves',
       'Sweet Deals'
     ]
-  }
+  },
+  domTrg: [
+    '.swiper-wrapper',
+    '.list-wrapper'
+  ],
+  tmps: [
+    templateSwpr,
+    templateGrid
+  ]
 }
 
-/**
+/** ***********************************************************
  * Data operations
- */
+ *************************************************************/
 
-var dataOps = function(data, keys) { // DOING:0 change to be able to request amount dynamically
-  console.log(data);
-  console.log(keys);
+// DOING:0 change to be able to request amount dynamically
 
+var dataOps = function(data, keys, iterator, selectors) {
   var model = [],
-      filtered;
+      filtered = [],
+      chunk = data.slice(1, iterator);
 
   for (var i = 0; i < keys.length; i++) {
-    filtered = data.filter(function(elem) {
-                return elem.category === keys[i];
-              });
+    var str = keys[i];
+    var slctr = selectors[i];
+    var obj = {}
+    obj.key = keys[i];
+    obj.selector = selectors[i];
+    obj[str] = chunk.filter(function(elem) {
+                 return elem.category === keys[i]
+               });
+    filtered.push(obj);
 
-    model.push(filtered);
+    model.push(obj);
   }
 
-  return renderSct(model);
+  return model;
 }
 
-
-/**
- * templating
- */
-
-// TODO break tmps off into seperate files
-
-var templateSwpr = '<li class="swiper-slide"><div class="card"><a class="card-link" href="#"><div class="card-media"></div><div class="item-info"><div class="item-name"><span class="bold">{{description}}</span></div><div class="item-sku"><span class="font_small uppercase">{{skuid}}</span></div><div class="item-price"><span class="bold">${{price}} /EA</span></div></div></a></div></li>';
-var templateGrid = '<li class="prod-grid__item"><div class="card"><a class="card-link" href="#"><div class="card-media"></div><div class="item-info"><div class="item-name"><span class="bold">{{description}}</span></div><div class="item-sku"><span class="font_small uppercase">{{skuid}}</span></div><div class="item-price"><span class="bold">${{price}} /EA</span></div></div></a></div></li>'
-
-var templatesArr = [templateSwpr, templateGrid];
+/** ***********************************************************
+* templating
+*************************************************************/
 
 var renderSct = function(model) {
   var sections = [],
       section;
 
   for (var i = 0; i < model.length; i++) {
-    section = renderTmp(model[i], templatesArr[i]);
+
+    section = renderTmp(model[i], cfg.tmps[i]);
 
     if (i === 1) {
       every5(section);
@@ -86,14 +96,19 @@ var renderSct = function(model) {
 
 
 var renderTmp = function(data, template) { // TODO:10 refactor
-  var output = [],
-      instance;
+  var output = {},
+      $arr = [],
+      $instance;
 
-  data.forEach(function(val) {
+  data[data.key].forEach(function(val) {
    rendering = Mustache.render(template, val);
-   instance = addBgImg(rendering, val);
-   output.push(instance);
+   $instance = addBgImg(rendering, val);
+   $arr.push($instance);
  })
+
+ output[data['key']] = $arr;
+ output.key = data.key;
+ output.selector = data.selector;
 
  return output;
 }
@@ -108,8 +123,8 @@ var addBgImg = function(elem, data) {
 }
 
 var every5 = function(arr) {
-  for (var i = arr.length; i > 0; i--) {
-    if ((i !== 20) && (i % 5 === 0)) {
+  for (var i = arr['key'].length; i > 0; i--) {
+    if ((i !== arr['key'].length) && (i % 5 === 0)) {
       addHorzRule(arr, i);
     }
   }
@@ -118,22 +133,22 @@ var every5 = function(arr) {
 }
 
 var addHorzRule = function(arr, idx) {
-  return arr.splice(idx, 0, $('<hr class="prod-grid__rule">'))
+  return arr[arr.key].splice(idx, 0, $('<hr class="prod-grid__rule">'))
 }
 
 
-/**
+/** ******************************************************************
  * Dom manipulation
- */
+ *******************************************************************/
 
 var insertTmp = function(input) { // TODO:20 refactor
   var targets = [
-    $('.swiper-wrapper'),
-    $('.list-wrapper')
+    $(cfg.domTrg[0]),
+    $(cfg.domTrg[1])
   ];
 
   for (var i = 0; i < targets.length; i++) {
-    input[i].forEach(function(val) {
+    input[i][input[i]['key']].forEach(function(val) {
       val.appendTo(targets[i]);
     })
   }
@@ -141,13 +156,25 @@ var insertTmp = function(input) { // TODO:20 refactor
   return targets;
 }
 
-/**
+/** *****************************************************************
  * Event Listeners
- */
+ ******************************************************************/
+
+
+/** *****************************************************************
+ * init
+ ******************************************************************/
+
+var init = function(data) {
+  var models = dataOps(data, cfg.ajx.prodCtg, 45, cfg.domTrg);
+  var tmps = renderSct(models);
+  var domEls = insertTmp(tmps);
+}
+
 
 $(document).ready(function () {
   $.get(cfg.ajx.url, function(data) {
-    insertTmp(dataOps(data, cfg.ajx.prodCtg));
+    init(data);
     return mySwiper = new Swiper('.swiper-container', cfg.swiper);
   }, 'json')
 })
---
+++ app/scripts/templates/prodCard.js
@@ -0,0 +1,44 @@
+/* eslint-disable */
+
+var tmpSwpr = '<li class="swiper-slide">'
+            +   '<div class="card">'
+            +     '<a class="card-link" href="#">'
+            +       '<div class="card-media"></div>'
+            +       '<div class="item-info">'
+            +         '<div class="item-name">'
+            +           '<span class="bold">{{description}}</span>'
+            +         '</div>'
+            +         '<div class="item-sku">'
+            +           '<span class="font_small uppercase">{{skuid}}</span>'
+            +         '</div>'
+            +         '<div class="item-price">'
+            +           '<span class="bold">{{price}}</span>'
+            +         '</div>'
+            +       '</div>'
+            +     '</a>'
+            +   '</div>'
+            + '</li>'
+
+var tmpGrid = '<li class="prod-grid__item">'
+            +   '<div class="card">'
+            +     '<a class="card-link" href="#">'
+            +       '<div class="card-media"></div>'
+            +       '<div class="item-info">'
+            +         '<div class="item-name">'
+            +           '<span class="bold">{{description}}</span>'
+            +         '</div>'
+            +         '<div class="item-sku">'
+            +           '<span class="font_small uppercase">{{skuid}}</span>'
+            +         '</div>'
+            +         '<div class="item-price">'
+            +           '<span class="bold">{{price}}</span>'
+            +         '</div>'
+            +       '</div>'
+            +     '</a>'
+            +   '</div>'
+            + '</li>'
+
+tmps = [
+  swpr = tmpSwpr,
+  prod = tmpGrid
+]
